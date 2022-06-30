0a77b5eb06aa68dbaeff3486b62ad13991d3ae75
RubelBiswasDH <RubelBiswasDH@gmail.com>
2022-06-30 19:29:48 +0600

    removed annoucement edit button

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -23,7 +23,6 @@ const columns = [
   { field: 'announced_time', headerName: 'Announced At', minWidth: 75, flex: .75, sortable: true, filter: false,filterable: false },
   { field: 'announcement', headerName: 'Announcement', minWidth: 100, sortable: false,flex: 1, filter: true, filterable: true  },
   { field: 'type', headerName: 'Type', minWidth: 50, sortable: true,flex: .5, filter: true, filterable: true  },
-  { field: 'edit', headerName: 'Edit', minWidth: 40, sortable: false,flex: .40, filter: false, filterable: false  },
 ]
 
 class Announcements extends React.PureComponent {
